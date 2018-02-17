8faeba40d3d5e21762fc0680bf97822c9862841b
elliot berggren <elliot.berggren@hotmail.com>
2018-02-17 21:35:46 +0100

    Calc first save point

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -10,6 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var additionSwitch: UISwitch!
+    
+    
+    
+    
+        @IBAction func buttonTapped(_ sender: Any) {
+            
+            let addition = additionSwitch.isOn
+            
+            if addition{
+            let sum = Double(topTextField.text!)! + Double(bottomTextField.text!)!
+            
+            myLabel.text = "\(topTextField.text!) + \(bottomTextField.text!) = \(sum)"
+            
+            }else{
+                
+            let sum = Double(topTextField.text!)! - Double(bottomTextField.text!)!
+                
+            myLabel.text = "\(topTextField.text!) - \(bottomTextField.text!) = \(sum)"
+                
+            }
+            
+    
+        }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -17,7 +46,7 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
 
 
